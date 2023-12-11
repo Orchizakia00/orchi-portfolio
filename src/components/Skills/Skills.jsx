163a27b46a8034350d47f8c8169a4fc495edb920
Orchizakia00 <zakiaislam290@gmail.com>
2023-12-11 11:36:53 +0600

    icons added in skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,5 +1,13 @@
+import { FaCss3, FaGit, FaGitSquare, FaHtml5, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
 import SectionTitle from "../Shared/SectionTitle/SectionTitle";
-
+import tailwind from "../../assets/icons/tailwind.png"
+import js from "../../assets/icons/js.png"
+import css from "../../assets/icons/css.png"
+import html from "../../assets/icons/html.png"
+import git from "../../assets/icons/git.png"
+import firebase from "../../assets/icons/firebase.png"
+import mongodb from "../../assets/icons/mongodb.png"
+import ex from "../../assets/icons/ex.png"
 
 const Skills = () => {
     return (
@@ -7,99 +15,110 @@ const Skills = () => {
             <SectionTitle heading={'Skills'}></SectionTitle>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 my-16">
 
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 70 }} role="progressbar">70%</div>
                     </div>
                     <div className="card-body text-center">
+                        <p className="flex justify-center"><FaReact className="" size={50} color="cyan"></FaReact></p>
                         <h2 className="font-bold text-2xl">React</h2>
                         <p>Intermediate</p>
                     </div>
                 </div>
 
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 70 }} role="progressbar">70%</div>
                     </div>
                     <div className="card-body text-center">
+
+                        <p className="flex justify-center"><img src={js} alt="" /></p>
                         <h2 className="font-bold text-2xl">JavaScript</h2>
                         <p>Intermediate</p>
                     </div>
                 </div>
 
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 90 }} role="progressbar">90%</div>
                     </div>
                     <div className="card-body text-center">
+                        <p className="flex justify-center"><img src={html} alt="" /></p>
                         <h2 className="font-bold text-2xl">HTML</h2>
                         <p>Advanced</p>
                     </div>
                 </div>
 
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 90 }} role="progressbar">90%</div>
                     </div>
                     <div className="card-body text-center">
+                        <p className="flex justify-center"><img src={css} alt="" /></p>
                         <h2 className="font-bold text-2xl">CSS</h2>
                         <p>Advanced</p>
                     </div>
                 </div>
 
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 90 }} role="progressbar">90%</div>
                     </div>
                     <div className="card-body text-center">
+                        <p className="flex justify-center"><img src={tailwind} alt="" /></p>
                         <h2 className="font-bold text-2xl">Tailwind CSS</h2>
                         <p>Advanced</p>
                     </div>
                 </div>
 
 
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 20 }} role="progressbar">20%</div>
                     </div>
                     <div className="card-body text-center">
+                    <p className="flex justify-center"><img src={mongodb} width={48} height={48} alt="" /></p>
                         <h2 className="font-bold text-2xl">MongoDB</h2>
                         <p>Basic</p>
                     </div>
                 </div>
 
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 70 }} role="progressbar">70%</div>
                     </div>
                     <div className="card-body text-center">
+                    <p className="flex justify-center"><img src={firebase} alt="" /></p>
                         <h2 className="font-bold text-2xl">Firebase</h2>
                         <p>Intermediate</p>
                     </div>
                 </div>
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 20 }} role="progressbar">20%</div>
                     </div>
                     <div className="card-body text-center">
+                        <p className="flex justify-center"><FaNodeJs size={40} ></FaNodeJs></p>
                         <h2 className="font-bold text-2xl">Node</h2>
                         <p>Basic</p>
                     </div>
                 </div>
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 70 }} role="progressbar">70%</div>
                     </div>
                     <div className="card-body text-center">
+                    <p className="flex justify-center"><img src={git} alt="" /></p>
                         <h2 className="font-bold text-2xl">Git</h2>
                         <p>Intermediate</p>
                     </div>
                 </div>
-                <div className="card card-compact w-56 bg-base-100 shadow-xl">
+                <div className="card card-compact w-56 bg-base-100 shadow-lg">
                     <div className="text-center mt-5">
                         <div className="radial-progress text-accent text-center" style={{ "--value": 20 }} role="progressbar">20%</div>
                     </div>
                     <div className="card-body text-center">
+                    <p className="flex justify-center"><img src={ex} alt="" /></p>
                         <h2 className="font-bold text-2xl">Express</h2>
                         <p>Basic</p>
                     </div>
