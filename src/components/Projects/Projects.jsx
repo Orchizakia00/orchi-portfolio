a0be99e606923ea27c6470777877817dc77a58f2
Tanzim Orchi <zakiaislam290@gmail.com>
2024-10-29 02:46:06 +0600

    new projects added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,8 @@
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import SectionTitle from "../Shared/SectionTitle/SectionTitle";
-
+import taskImg from "../../assets/projects/taskImg1.png"
+import shopImg from "../../assets/projects/shopImg1.png"
 
 const Projects = () => {
     return (
@@ -9,6 +10,17 @@ const Projects = () => {
             <SectionTitle heading={'Projects'}></SectionTitle>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-20 lg:w-full md:w-full w-[90%] mx-auto">
+                <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src={shopImg} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Rabeya Mart</h2>
+                        <p>Online Grocery Shop (Ongoing) </p>
+                        <div className="card-actions justify-end">
+                            <Link to={'/project-three'}><button className="btn btn-accent">Details <FaArrowRight /> </button></Link>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="card card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/VvBTvmt/Screenshot-2023-12-11-132631.png" alt="Shoes" /></figure>
                     <div className="card-body">
@@ -19,26 +31,23 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
+
                 <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img src="https://i.ibb.co/6bRKBBr/Screenshot-2023-12-11-162443.png" alt="Shoes" /></figure>
+                    <figure><img src={taskImg} alt="Shoes" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title">Team Tracker</h2>
-                        <p>Employee Management System</p>
+                        <h2 className="card-title">Task Sync Pro</h2>
+                        <p>Task Management System</p>
                         <div className="card-actions justify-end">
                             <Link to={'/project-two'}><button className="btn btn-accent">Details <FaArrowRight /> </button></Link>
                         </div>
                     </div>
                 </div>
-                <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                    <figure><img src="https://i.ibb.co/NNrKcDV/Screenshot-2023-12-11-182710.png" alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Tasty Hub</h2>
-                        <p>E-commerce Website</p>
-                        <div className="card-actions justify-end">
-                            <Link to={'/project-three'}><button className="btn btn-accent">Details <FaArrowRight /> </button></Link>
-                        </div>
-                    </div>
-                </div>
+
+            </div>
+            <div className="flex justify-center">
+                <a href="https://github.com/Orchizakia00?tab=repositories" target="_blank" rel="noopener noreferrer">
+                    <button className="btn btn-accent">More Projects</button>
+                </a>
             </div>
         </div>
     );
